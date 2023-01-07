1fff5fcb9d64573d0526c4b8b6ede2166ff3a950
jacobjameson <jacobjameson@g.harvard.edu>
2023-01-07 18:38:15 -0500

    finished? problems with subtitle

--- 2023/2023-01-03/week1.R
+++ 2023/2023-01-03/week1.R
@@ -16,6 +16,8 @@ if (any (installed_libs == F)) {
 
 invisible(lapply (libs, library, character.only = T))
 
+library(showtext)
+font_add_google("Lato")
 showtext_auto()
 
 # load dataset ------------------------------------------------------------
@@ -25,21 +27,6 @@ rent.raw <- tuesdata$rent
 
 # wrangle data ------------------------------------------------------------
 
-cities.2008 <- rent.raw %>%
-  mutate(city = str_to_title(city)) %>%
-  filter(year == 2008)
-
-cities.total <- rent.raw %>%
-  mutate(city = str_to_title(city))
-
-cities.2008 <- unique(cities.2008$city)
-total.cities <- unique(cities.total$city)
-
-cities.drop <- !(total.cities %in% cities.2008)
-
-total.cities[cities.drop]
-  
-
 rent <- rent.raw %>%
   filter(beds == 2) %>%
   select(year, beds, price, city) %>%
@@ -48,13 +35,19 @@ rent <- rent.raw %>%
   summarise(price = mean(price)) %>%
   ungroup()
 
+cities.included <- rent %>%
+  group_by(city) %>%
+  summarise(n = n()) %>%
+  filter(n > 16)
 
-length(unique(rent$city))
+cities.included <- unique(cities.included$city)
 
+rent <- rent %>%
+  filter(city %in% cities.included)
 
 # Also define the group of cities that are going to be highlighted
-highlights <- c("San Francisco", "Berkeley", "Santa Cruz", 
-                "Watsonville", " Palo Alto", "Half Moon Bay", "Oakland")
+highlights <- c("San Francisco", "Berkeley", "Menlo Park", 
+                "Palo Alto", " San Jose")
 
 n <- length(highlights)
 
@@ -66,42 +59,51 @@ rent <- rent %>%
          price_rel = price - price_index,
     group = if_else(city %in% highlights, city, "other"),
     group = as.factor(group)) %>% 
-  mutate(
-    group = fct_relevel(group, "other", after = Inf),
-    name_lab = if_else(year == 2018, name, NA_character_)) %>% 
+    mutate(
+      group = fct_relevel(group, "other", after = Inf),
+      name_lab = if_else(year == 2018, city, NA_character_)) %>% 
   ungroup()
 
-
-rent %>% filter(city == "Ben Lomond")
 
 # theme --------------------------------------------------------------------
 
 theme_set(theme_minimal(base_family = "Lato"))
 
 theme_update(
+  # Remove title for both x and y axes
   axis.title = element_blank(),
+  # Axes labels are grey
   axis.text = element_text(color = "grey40"),
+  # The size of the axes labels are different for x and y.
   axis.text.x = element_text(size = 20, margin = margin(t = 5)),
   axis.text.y = element_text(size = 17, margin = margin(r = 5)),
+  # Also, the ticks have a very light grey color
   axis.ticks = element_line(color = "grey91", size = .5),
+  # The length of the axis ticks is increased.
   axis.ticks.length.x = unit(1.3, "lines"),
   axis.ticks.length.y = unit(.7, "lines"),
+  # Remove the grid lines that come with ggplot2 plots by default
   panel.grid = element_blank(),
+  # Customize margin values (top, right, bottom, left)
   plot.margin = margin(20, 40, 20, 40),
+  # Use a light grey color for the background of both the plot and the panel
   plot.background = element_rect(fill = "grey98", color = "grey98"),
   panel.background = element_rect(fill = "grey98", color = "grey98"),
+  # Customize title appearence
   plot.title = element_text(
     color = "grey10", 
     size = 28, 
     face = "bold",
     margin = margin(t = 15)
   ),
-  plot.subtitle = element_markdown(
+  # Customize subtitle appearence
+  plot.subtitle = element_text(
     color = "grey30", 
     size = 16,
     lineheight = 1.35,
-    margin = margin(t = 15, b = 40)
+    margin = margin(t = 15)
   ),
+  # Title and caption are going to be aligned
   plot.title.position = "plot",
   plot.caption.position = "plot",
   plot.caption = element_text(
@@ -109,57 +111,60 @@ theme_update(
     size = 13,
     lineheight = 1.2, 
     hjust = 0,
-    margin = margin(t = 40)
+    margin = margin(t = 40) # Large margin on the top of the caption.
   ),
+  # Remove legend
   legend.position = "none"
 )
 
+
 # plot --------------------------------------------------------------------
 
+rm(plt)
+plt <- ggplot(rent %>% filter(group != "other"), 
+              aes(year, price_rel, group = city)) + 
+  geom_vline(xintercept = seq(2000, 2018, by = 3),
+             color = "grey91", size = .6) +
+  geom_segment( data = tibble(y = seq(-1500, 3000, by = 250), 
+                              x1 = 2000, x2 = 2018),
+                aes(x = x1, xend = x2, y = y, yend = y),
+                inherit.aes = FALSE, color = "grey91", size = .6) +
+  geom_segment(data = tibble(y = 0, x1 = 2000, x2 = 2018),
+               aes(x = x1, xend = x2, y = y, yend = y),
+               inherit.aes = FALSE, color = "grey60", size = .8) +
+  geom_vline(aes(xintercept = 2008), color = "grey40", linetype = "dotted",
+             size = .8) +
+  geom_line(data = rent %>% filter(group == "other"), 
+            color = "grey75", size = .6, alpha = .5) +
+  geom_line(aes(color = city), size = .9)
 
-plt <- ggplot(
-  # The ggplot object has associated the data for the highlighted countries
-  rent, 
-  aes(year, median.price, group = city)
-) + 
-  # Geometric annotations that play the role of grid lines
-  geom_vline(
-    xintercept = seq(2000, 2020, by = 5),
-    color = "grey91", 
-    size = .6
-  ) +
-  geom_segment(
-    data = tibble(y = seq(-4, 3, by = 1), x1 = 2000, x2 = 2020),
-    aes(x = x1, xend = x2, y = y, yend = y),
-    inherit.aes = FALSE,
-    color = "grey91",
-    size = .6
-  ) +
-  geom_segment(
-    data = tibble(y = 0, x1 = 2000, x2 = 2020),
-    aes(x = x1, xend = x2, y = y, yend = y),
-    inherit.aes = FALSE,
-    color = "grey60",
-    size = .8
-  ) +
-  geom_vline(
-    aes(xintercept = 2008), 
-    color = "grey40",
-    linetype = "dotted",
-    size = .8
-  ) +
-  ## Lines for the non-highlighted countries
-  geom_line(
-    data = rent,
-    color = "grey75",
-    size = .6,
-    alpha = .5
-  ) +
-  ## Lines for the highlighted countries.
-  # It's important to put them after the grey lines
-  # so the colored ones are on top
-  geom_line(
-    aes(color = city),
-    size = .9
+plt
+
+
+plt <- plt + 
+  annotate("text", x = 2008.15, y = -1000, label = "2008",
+           family = "Lato", size = 8, color = "grey40",
+           hjust = 0) +
+  geom_text_repel(aes(color = group, label = name_lab), fontface = "bold", 
+                  size = 8, direction = "y", xlim = c(2018.8, NA),
+                  hjust = 0, segment.size = .7, segment.alpha = .5,
+                  segment.linetype = "dotted", box.padding = .4,
+                  segment.curvature = -0.1, segment.ncp = 3,
+                  segment.angle = 20) + 
+  coord_cartesian(clip = "off", ylim = c(-1500, 3000)) +
+  scale_x_continuous(expand = c(0, 0), limits = c(2000, 2021.5), 
+                     breaks = seq(2000, 2018, by = 3)) +
+  scale_y_continuous(expand = c(0, 0), breaks = seq(-1500, 3000, by = 500),
+                     labels = glue::glue("{format(seq(-1500, 3000, by = 500), nsmall = 2)}$")) 
+
+plt 
+
+plt <- plt + 
+  scale_color_manual(
+    values = c(rcartocolor::carto_pal(n = n, name = "Bold")[1:n-1], "grey50")) +
+  labs(
+    title = "Compared to the financial crisis in 2008, 2 bedroom prices skyrocket in the Bay",
+    subtitle = "The index chart visualizes the price changes (in USD) of a 2 bedroom housing unit in the Bay Area of California based on a 2008 as index year.",
+    caption = "Visualization by Jacob Jameson (@JacobCJameson) • 2023 Week 1 of #TidyTuesday, 'Bring your own data from 2022!' (07-05-2022) • Inspired by visualization by Cédric Scherer."
   )
 plt
