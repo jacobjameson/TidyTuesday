bcbe4765ef9a90ffd0ad8c034d04a140747a809a
jacobjameson <jacobjameson@g.harvard.edu>
2023-01-23 11:24:09 -0500

    finished

--- 2023/2023-01-24/week4.R
+++ 2023/2023-01-24/week4.R
@@ -39,6 +39,49 @@ survivalists <- survivalists %>%
 
 data <- merge(survivalists, episodes, by=c('season', 'episode'))
 
+
+df_episodes_avg <-
+  data %>% 
+  arrange(season, episode) %>% 
+  mutate(episode_id = row_number()) %>% 
+  group_by(season) %>% 
+  mutate(
+    avg = mean(days_lasted),
+    episode_mod = episode_id + (9 * season),
+    mid = mean(episode_mod)) %>% 
+  ungroup() %>% 
+  mutate(season = factor(season))
+
+
+df_lines <-
+  df_episodes_avg %>% 
+  group_by(season) %>% 
+  summarize(
+    start_x = min(episode_mod) - 5,
+    end_x = max(episode_mod) + 5,
+    y = unique(avg)
+  ) %>% 
+  pivot_longer(
+    cols = c(start_x, end_x),
+    names_to = "type",
+    values_to = "x"
+  ) %>% 
+  mutate(
+    x_group = if_else(type == "start_x", x + .1, x - .1),
+    x_group = if_else(type == "start_x" & x == min(x), x_group - .1, x_group),
+    x_group = if_else(type == "end_x" & x == max(x), x_group + .1, x_group)
+  )
+
+library(png)
+library(patchwork)
+library(grid)
+
+background <- png::readPNG("alone.png")
+w <- matrix(rgb(background[,,1],background[,,2],
+                background[,,3], background[,,4] * 0.05),
+            nrow=dim(background)[1]) 
+
+
 # theme --------------------------------------------------------------------
 
 theme_set(theme_minimal(base_family = "Pragati Narrow"))
@@ -78,48 +121,15 @@ legend.title.align=0.5,
   plot.caption.position = "plot",
   plot.caption = element_text(
     color = "grey20", 
-    size = 15,
+    size = 18,
     lineheight = 1.2, 
     hjust = 0.5,
-    margin = margin(t = 30) 
+    margin = margin(t = 50) 
   ))
 
 
 
 # plot --------------------------------------------------------------------
-
-
-df_episodes_avg <-
-  data %>% 
-  arrange(season, episode) %>% 
-  mutate(episode_id = row_number()) %>% 
-  group_by(season) %>% 
-  mutate(
-    avg = mean(days_lasted),
-    episode_mod = episode_id + (9 * season),
-    mid = mean(episode_mod)) %>% 
-  ungroup() %>% 
-  mutate(season = factor(season))
-
-
-df_lines <-
-  df_episodes_avg %>% 
-  group_by(season) %>% 
-  summarize(
-    start_x = min(episode_mod) - 5,
-    end_x = max(episode_mod) + 5,
-    y = unique(avg)
-  ) %>% 
-  pivot_longer(
-    cols = c(start_x, end_x),
-    names_to = "type",
-    values_to = "x"
-  ) %>% 
-  mutate(
-    x_group = if_else(type == "start_x", x + .1, x - .1),
-    x_group = if_else(type == "start_x" & x == min(x), x_group - .1, x_group),
-    x_group = if_else(type == "end_x" & x == max(x), x_group + .1, x_group)
-  )
 
 
 p <- df_episodes_avg %>% 
@@ -130,7 +140,7 @@ p <- df_episodes_avg %>%
     color = "grey82",
     size = .5
   )
-p
+
 
 
 p <- p + 
@@ -164,7 +174,7 @@ p <- p +
     aes(color = season)
   ) 
 
-p
+
 
 p <- p + 
   geom_label(
@@ -183,8 +193,6 @@ p <- p +
     label.size = NA
   ) 
 
-# Scale and labels customization.
-# Override default colors with a much better looking palette.
 p <- p + 
   scale_y_continuous(
     limits = c(-15, 110),
@@ -195,9 +203,15 @@ p <- p +
                "#000000", "#0072B2", "#D55E00", "#CC79A7"),
     guide = FALSE 
   ) +
-  labs(caption= str_wrap("This dataset contains data from the TV series Alone collected and shared by Dan Oehm. As described in Oehm's blog post, in the survival TV series ‘Alone,’ 10 survivalists are dropped in an extremely remote area and must fend for themselves. They aim to last 100 days in the Artic winter, living off the land through their survival skills, endurance, and mental fortitude • Visualization by @JacobCJameson", 220),
+  labs(caption= str_wrap("Data comes from the TV series Alone, 
+                         collected and shared by Dan Oehm. As described in Oehm's blog post, 
+                         in the survival TV series ‘Alone’, 10 survivalists are dropped in 
+                         an extremely remote area and must fend for themselves. 
+                         They aim to last 100 days in the Artic winter, living off the 
+                         land through their survival skills, endurance, and mental 
+                         fortitude • Visualization by @JacobCJameson", 200),
        subtitle= "A Season-by-Season Breakdown of ALONE Contestant Longevity",
-       title="SURVIVING SOLITUDE", fill="",
+       title="SURVIVING SOLITUDE",
        ylab = 'Days Survived') +
   guides(
     size = guide_bins(
@@ -205,7 +219,7 @@ p <- p +
       direction = "horizontal",
       title.position = "top",
       title.hjust = .5)) +
-  geom_label(aes(x=130,y=105, label = "Roland Welker made it 100 days"), size=5,
+  geom_label(aes(x=129,y=100, label = "Roland Welker made it 100 days"), size=5,
              hjust = 0, vjust = "inward",
              nudge_x = 0.05, nudge_y = 2,
              label.padding = unit(0.2, "lines"),
@@ -214,10 +228,10 @@ p <- p +
              hjust = 0, vjust = "inward",
              nudge_x = 0.05, nudge_y = 2,
              label.padding = unit(0.2, "lines"), 
-             label.size = NA, fill='#f5f5f2', color='#9986A5')
+             label.size = NA, fill='#f5f5f2', color='#9986A5') +
+  annotation_custom(xmin=-Inf, ymin=-Inf, xmax=Inf, ymax=Inf, rasterGrob(w)) 
 
 p
-
 
 
 
